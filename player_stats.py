4a9f97abd3bfd6697771783f6612322391ab983e
Jelani Alexis <31781@ma-web.nl>
2020-09-30 11:45:30 +0200

    Fixed

---
+++ player_stats.py
@@ -0,0 +1,15 @@
+name = "Lord Mighty"
+health = 150
+attack = 35
+defense = 10
+atkBuff = False
+defBuff = True
+speed = "Medium"
+
+print("Name:", name)
+print("Base Health:", health)
+print("Attack Damage:", attack)
+print("Defense:", defense)
+print("Attack Buff:", atkBuff)
+print("Defense Buff:", defBuff)
+print("Movement Speed:", speed)
