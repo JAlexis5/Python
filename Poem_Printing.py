c2801cdd7a349ccf6dc700cdb0cf7070a4d3a2d4
Jelani Alexis <31781@ma-web.nl>
2020-09-30 11:43:37 +0200

    fix

---
+++ Poem_Printing.py
@@ -0,0 +1,17 @@
+vers1 = "Ik ben Jelani Alexis"
+vers2 = "Hopelijk rijd ik later een Lexus"
+vers3 = "Ik woon sinds ik ben geboren,"
+vers4 = "In Haarlem dichtbij een toren"
+vers5 = "Ik game vaak als ik vrij ben,"
+vers6 = "Daarvan ben ik echt een fan"
+vers7 = "Ik heb voor de rest eigenlijk niks"
+vers8 = "Ik ga weer verder met het fixen van chicks"
+
+print(vers1)
+print(vers2)
+print(vers3)
+print(vers4)
+print(vers5)
+print(vers6)
+print(vers7)
+print(vers8)
