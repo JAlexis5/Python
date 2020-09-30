76781ff8331b10c51c59e70ed3a3e2dadabcb8eb
Jelani Alexis <31781@ma-web.nl>
2020-09-30 12:24:38 +0200

    FamilyTies.py

---
+++ FamilyTies.py
@@ -0,0 +1,9 @@
+name = "Jelani."
+age = "15"
+placeResidence = "Haarlem"
+schoolDistanceKm = "13.9"
+familySize = "3"
+coronaStatus = "geen"
+
+print("Ik ben " + name + " Ik ben " + age + " jaar oud. Ik woon in " + placeResidence + " in Schalkwijk. Ik woon " + schoolDistanceKm + " km van school af. Mijn gezin bestaat uit " + familySize + " mensen, mijn moeder, mijn broer en ik. Een laatste feitje: ik heb " + coronaStatus + " corona.")
+
